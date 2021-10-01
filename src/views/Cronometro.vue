32a8ccb61c170c182e5f9b65e748b6e237ac4161
anderrf <anderson_rocha_farias@outlook.com>
2021-10-01 16:09:06 -0300

    Funcionalidade do Cronometro

--- src/views/Cronometro.vue
+++ src/views/Cronometro.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-row>
             <v-col>
-                <v-text-field class="centered-input" solo-inverted disabled :label="`${(hours < 10) ? ('0'+  hours) : hours}:${(minutes < 10) ? ('0'+  minutes) : minutes}:${(seconds < 10) ? ('0'+  seconds) : seconds}`"></v-text-field>
+                <v-text-field class="centered-input" solo-inverted disabled :value="`${(hours < 10) ? ('0'+  hours) : hours}:${(minutes < 10) ? ('0'+  minutes) : minutes}:${(seconds < 10) ? ('0'+  seconds) : seconds}`"></v-text-field>
             </v-col>
         </v-row>
         <v-row>
@@ -15,7 +15,7 @@
                 </v-btn>
             </v-col>
             <v-col>
-                <v-btn block color="secondary">
+                <v-btn block color="secondary" @click="clearTime()">
                     <v-icon>mdi-timer-sand-empty</v-icon>
                 </v-btn>
             </v-col>
@@ -30,18 +30,52 @@
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
-                running: false
+                running: false,
+                interval: null
             }
+        },
+        created(){
+            this.clearTime();
         },
         methods: {
             stopOrPlay(){
                 this.running = !this.running;
+                if(this.running){
+                    this.startRun();
+                }
+                else{
+                    this.stopRun();
+                }
+            },
+            clearTime(){
+                this.hours = this.minutes = this.seconds = 0;
+                if(this.running){
+                    this.stopRun();
+                    this.startRun();
+                }
+            },
+            startRun(){
+                this.interval = setInterval(this.addTime, 1000);
+            },
+            stopRun(){
+                clearInterval(this.interval);
+            },
+            addTime(){
+                this.seconds++;
+                if(this.seconds === 60){
+                    this.seconds = 0;
+                    this.minutes++;
+                }
+                if(this.minutes === 60){
+                    this.minutes = 0;
+                    this.hours++;
+                }
             }
         }
     }
 </script>
 <style scoped>
-    .centered-input >>> input {
-      text-align: center
+    .centered-input >>> input{
+        text-align: center;
     }
 </style>
